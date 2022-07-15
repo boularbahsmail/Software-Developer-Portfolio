ffd8206ee1df44d1b1c81d55d1793efe7a500019
boularbahsmail <qiranorane55@gmail.com>
2022-07-15 14:34:43 +0100

    Add More Works(Projects) Cards #1 :+1:

--- components/Works/data/data.js
+++ components/Works/data/data.js
@@ -19,7 +19,28 @@ const data = [
         work_title: "Site Optimization",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         genre: "Interactive guide"
-    }
+    },
+    {
+        id: 4,
+        image_url: "https://cdn.dribbble.com/users/2417352/screenshots/6918204/media/d88f537661e3ea9cc4802e31b06e0ad8.png",
+        work_title: "Site Optimization",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        genre: "Interactive guide"
+    },
+    {
+        id: 5,
+        image_url: "https://cdn.dribbble.com/users/2919663/screenshots/18372585/media/22afc4757735732a96d5a9563202f729.png",
+        work_title: "Blockchain Platform",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        genre: "Digital Assets"
+    },
+    {
+        id: 6,
+        image_url: "https://cdn.dribbble.com/users/702789/screenshots/15916650/media/ec203705bbc00f7338ba4e356b2c8af9.png",
+        work_title: "Web Application",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        genre: "Design & Dev"
+    },
 ];
 
 export default data;
--- components/Works/index.jsx
+++ components/Works/index.jsx
@@ -27,10 +27,10 @@ const Works = () => {
                                 </div>
                                 <div className="flex flex-col justify-center items-start w-55 pb-0">
                                     <h2 className="text-2xl text-white font-bold my-5 mb-2">{work.work_title}</h2>
-                                    <b className="text-teal-500 mb-2">{work.genre}</b>
-                                    <p className="text-gray-400 leading-6 m-0">{work.description}</p>
-                                    <button className="py-2 px-4 bg-white rounded-md mt-4" style={{width: "100%"}} title="Visit website">
-                                        <a href="" className="flex justify-between items-center gap-1 font-semibold text-black text-md p-0 m-0">
+                                    <b className="text-teal-500 mb-2">⎯⎯ {work.genre}</b>
+                                    <p className="text-gray-300 text-sm leading-6 m-0">{work.description}</p>
+                                    <button className="py-2 px-4 bg-white mt-4 text-black ease-in-out duration-150 border-2 border-white rounded-md hover:bg-gray-900 hover:border-gray-900 hover:text-white" style={{width: "100%"}} title="Visit website">
+                                        <a href="" className="flex justify-between items-center gap-1 font-semibold text-md p-0 m-0">
                                             <span>Visit website</span>
                                             <HiOutlineArrowNarrowRight />
                                         </a>
