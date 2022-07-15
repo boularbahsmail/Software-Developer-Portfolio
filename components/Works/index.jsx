8daf39c2fff122599e62401e84a6e70f55aa43a7
boularbahsmail <qiranorane55@gmail.com>
2022-07-15 14:19:43 +0100

    Hover Effect On Projects Cards | Visit website button re-styled :+1:

--- components/Works/index.jsx
+++ components/Works/index.jsx
@@ -18,7 +18,7 @@ const Works = () => {
                 {
                     works ?
                         works.map((work, index) => 
-                            <div key={index} title={`${work.work_title} - ${work.genre}`} className="bg-cyan-900 rounded-lg p-4 max-w-4xl m-auto mb-4 w-full grid grid-cols-1 gap-0 sm:mb-0">
+                            <div key={index} title={`${work.work_title} - ${work.genre}`} className="bg-cyan-900 rounded-lg p-4 max-w-4xl m-auto mb-4 w-full grid grid-cols-1 gap-0 ease-in-out duration-150 hover:bg-cyan-800 sm:mb-0">
                                 <div className="w-45 felx justify-center items-center">
                                     <img 
                                         className="rounded-lg w-100" 
@@ -29,8 +29,8 @@ const Works = () => {
                                     <h2 className="text-2xl text-white font-bold my-5 mb-2">{work.work_title}</h2>
                                     <b className="text-teal-500 mb-2">{work.genre}</b>
                                     <p className="text-gray-400 leading-6 m-0">{work.description}</p>
-                                    <button className="py-2 px-4 bg-cyan-800 rounded-md mt-4" style={{width: "100%"}} title="Visit website">
-                                        <a href="" className="flex justify-between items-center gap-1 font-semibold text-white text-md p-0 m-0">
+                                    <button className="py-2 px-4 bg-white rounded-md mt-4" style={{width: "100%"}} title="Visit website">
+                                        <a href="" className="flex justify-between items-center gap-1 font-semibold text-black text-md p-0 m-0">
                                             <span>Visit website</span>
                                             <HiOutlineArrowNarrowRight />
                                         </a>
